3c9f3d2fe05765506f57f35b7f86d1f6046a0597
donivanjames <donivanjames@gmail.com>
2023-01-17 18:14:33 -0500

    Redid player

--- scripts/introScene.js
+++ scripts/introScene.js
@@ -4,8 +4,8 @@ import { setupGame, sequence3 } from "../script.js"
 let lastTime = null
 let introSpeed = 0.025
 let waitAtEnd = 1000
-const PLAYER_FRAME_COUNT = 4; // amount of animation frames
-const FRAME_TIME = 100; // how long each animation frame should last (in milliseconds)
+const PLAYER_FRAME_COUNT = 2; // amount of animation frames
+const FRAME_TIME = 140; // how long each animation frame should last (in milliseconds)
 let playerFrame = 0
 let currentFrameTime = 0
 
@@ -65,7 +65,7 @@ export function handleRun(delta) {
     if (currentFrameTime >= FRAME_TIME) {
       // swaps animation frames when currentFrameTime is above frameTime
       playerFrame = (playerFrame + 1) % PLAYER_FRAME_COUNT; // will cycle animation frames no matter how many there are
-      player.src = `imgs/kid-run${playerFrame}.png`; // picks an image from the current player frame
+      player.src = `imgs/kid-walk-${playerFrame}.png`; // picks an image from the current player frame
       currentFrameTime = 0; // reset currentFrameTime back to 0
       // currentFrameTime -= FRAME_TIME; // used to be this, if there's ever more than two frames you might need this
     }
--- scripts/player.js
+++ scripts/player.js
@@ -4,8 +4,8 @@ import { jumpSound, loseSound } from "./audioManager.js";
 const player = document.querySelector("[data-player]");
 const JUMP_SPEED = 0.30;
 const GRAVITY = 0.0015;
-const PLAYER_FRAME_COUNT = 4; // amount of animation frames
-const FRAME_TIME = 90; // how long each animation frame should last (in milliseconds)
+const PLAYER_FRAME_COUNT = 2; // amount of animation frames
+const FRAME_TIME = 140; // how long each animation frame should last (in milliseconds)
 
 const heightFromGround = 41 // also change css --bottom to match
 const jumpableHeight = 20
@@ -21,7 +21,7 @@ export function setupPlayer(environment) {
   playerFrame = 0;
   currentFrameTime = 0;
   yVelocity = 0;
-  player.src = `imgs/kid-idle.png`;
+  player.src = `imgs/kid-walk-0.png`;
 
   // Need a different zindex for the school so player falls into puddle and not behind it
   player.classList.remove("zindex-top")
@@ -78,14 +78,14 @@ export function setPlayerLose() {
 function handleRun(delta, speedScale) {
   if (isJumping) {
     // if isJumping: set animation to stationary
-    player.src = `imgs/kid-run1.png`;
+    player.src = `imgs/kid-jump.png`;
     return;
   }
 
   if (currentFrameTime >= FRAME_TIME) {
     // swaps animation frames when currentFrameTime is above frameTime
     playerFrame = (playerFrame + 1) % PLAYER_FRAME_COUNT; // will cycle animation frames no matter how many there are
-    player.src = `imgs/kid-run${playerFrame}.png`; // picks an image from the current player frame
+    player.src = `imgs/kid-walk-${playerFrame}.png`; // picks an image from the current player frame
     currentFrameTime = 0; // reset currentFrameTime back to 0
     // currentFrameTime -= FRAME_TIME; // used to be this, if there's ever more than two frames you might need this
   }
