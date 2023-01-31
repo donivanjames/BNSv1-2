09c32627c5cb082eceb608706e7d8970e1ba6719
donivanjames <donivanjames@gmail.com>
2023-01-31 15:05:30 -0500

    Background changes

--- scripts/ground.js
+++ scripts/ground.js
@@ -7,7 +7,7 @@ import {
 } from "./updateCustomProperty.js";
 
 const groundElems = document.querySelectorAll("[data-ground]");
-const totalBackgroundImages = 1 // for randomizing background
+const totalBackgroundImages = 3 // for randomizing background
 
 export function showGround() {
   groundElems.forEach((ground) => {
@@ -21,7 +21,7 @@ export function hideGround() {
   });
 }
 
-const groundWidth = 150; // if you change this make sure to change .ground width in css
+const groundWidth = 294; // if you change this make sure to change .ground width in css
 export function resetGround() {
   setCustomProperty(groundElems[0], "--left", 0);
   setCustomProperty(groundElems[1], "--left", groundWidth); //we have our width at 300% so 300 is what we set our starting value as for the second ground piece
