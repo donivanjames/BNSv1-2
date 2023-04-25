d920bc3d2e196e911caaea927521d93ec779bdde
donivanjames <donivanjames@gmail.com>
2023-04-25 16:36:56 -0400

    Update introScene.js

--- scripts/introScene.js
+++ scripts/introScene.js
@@ -104,6 +104,7 @@ function setupIntro() {
   // Rest of code needs to come after imgHeight, hence why they're not in window.matchMedia
 
   if(isChrome) console.log("using chrome")
+  else console.log("Not chrome")
   scrollSpeed = imgHeight * 0.00006;
   customPlayerPos = 0.894; // higher num = lower position
   customTutTextPost = 0.826;
