ea880cefd7e7cafb87a3004df034bfc4c9798872
donivanjames <donivanjames@gmail.com>
2023-04-28 18:41:47 -0400

    height fixes

--- scripts/title-text-html.js
+++ scripts/title-text-html.js
@@ -1,33 +1,38 @@
 export const GameTitleMain = `
 
-    <img
-    src="imgs/title-screen-caution-sign.png"
-    class="upper-image-404" 
-    />
-    <br />
+    
     <!-- BIG BNS LOGO -->
-    <img
-        src="imgs/bns-title-logo-5.png"
-        class="start-screen-title-img"
-    />
+    <div>
+        <img
+            src="imgs/title-screen-caution-sign.png"
+            class="upper-image-404" 
+        />
+        <br />
+        <img
+            src="imgs/bns-title-logo-5.png"
+            class="start-screen-title-img"
+        />
+    </div>
   `
 
 export const GameTitle404 = `
-    <!-- Upper Text -->
 
-    <img
-        src="imgs/404-error.png"
-        class="upper-image-404" 
-    />
-    <br />
-    <!-- BIG BNS LOGO -->
-    <img
-        src="imgs/bns-title-logo-404-1.png"
-        class="start-screen-title-img"
-    />
+    <div>
+        <img
+            src="imgs/404-error.png"
+            class="upper-image-404" 
+        />
+        <br />
+        <img
+            src="imgs/bns-title-logo-5.png"
+            class="start-screen-title-img"
+        />
+    </div>
+
+
     <br /><br />
     <!-- Had to tighten up the letter spacing for the words 'Stay' and 'Play'-->
-    <div class='title-text'>
+    <div class='title-text title-text-lower'>
         <div style="display: inline; letter-spacing: -.1rem;">STAY</div> AND <div style="display: inline; letter-spacing: -.06rem;">PLAY</div> 
     </div>
     <br />
